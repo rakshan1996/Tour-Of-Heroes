6b86eeab5344f24c344f26efb64bd6ea9b01b387
Rakshan Thapar <rakshanthapar32@gmail.com>
2018-11-18 18:54:02 +0530

    started with HTTP client and in memory web api

--- TourOfHeroes/src/app/hero-detail/hero-detail.component.ts
+++ TourOfHeroes/src/app/hero-detail/hero-detail.component.ts
@@ -22,9 +22,9 @@ export class HeroDetailComponent implements OnInit {
     //this.getHero ();
   }
 
-  getHero(): void {
-    const id = +this.route.snapshot.paramMap.get('id');
+  //getHero(): void {
+   // const id = +this.route.snapshot.paramMap.get('id');
   //this.heroService.getHero(id).subscribe(hero => this.hero = hero);
-  }
+  //}
 
 }
